0bc5b58de6c8c319494124eb933bf06ba7244517
mahesh945 <mp.mahesh2200@gmail.com>
2022-04-22 19:12:36 +0530

    JS basics

---
+++ TinDog-Start-master/leap-year.js
@@ -0,0 +1,17 @@
+function isLeap(){
+    if(year % 4==0){
+        if (year % 100 != 0) {
+            if (year % 400 == 0) {
+                return "it is leap year";
+            }
+        }
+    }
+    else{
+        return "not leap year";
+    }
+}
+
+studName = {Sara, Akash, Josy, Harry, Maya};
+for (let index = 0; index < studName.length; index++) {
+    print(StudName);    
+}
