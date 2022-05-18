07d97c6acdafc0de30727691ad1753c5252cfe69
mahesh945 <mp.mahesh2200@gmail.com>
2022-05-18 20:21:41 +0530

    objects

---
+++ render-using-operator/src/components/App.jsx
@@ -0,0 +1,15 @@
+const currentTime = new Date(2019, 11, 1, 9).getHours();
+console.log(currentTime);
+
+function App() {
+  return (
+    <div className="container">
+      {/*Ternary Operator*/}
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {/*AND Operator*/}
+      {currentTime > 12 && <h1>Why are you still working?</h1>}
+    </div>
+  );
+}
+
+export default App;
---
+++ render-using-operator/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+var count = 0 ;
+
+ReactDOM.render(
+    <div className="container">
+    <h1>{count}</h1>
+    <button>+</button>
+    </div> 
+     );
+
+
+
+
