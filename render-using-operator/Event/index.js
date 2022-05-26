739fd3e46e9dec10dfcf660e30662e07ced849ad
mahesh945 <mp.mahesh2200@gmail.com>
2022-05-26 20:09:06 +0530

    temp. literals

--- render-using-operator/Event/index.js
+++ render-using-operator/Event/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const fName = "Angela";
-const lName = "Yu";
-const num = 7;
+const fName = "Mahesh";
+const lName = "Patro";
+const num = 98;
+const currentDate = new Date();
+const year = currentDate.getFullYear();
 
 ReactDOM.render(
   <div>
     <h1>Hello {fName + " " + lName}!</h1>
     <p>Your lucky number is {num}</p>
+    <p>Copyright {year}</p>
+    <p>Copyright {currentDate.getFullYear()}</p>
   </div>,
   document.getElementById("root")
 );
