aa396084fc9fcd72e78b0aae4c435ae55e6d343f
mahesh945 <mp.mahesh2200@gmail.com>
2022-06-05 20:08:05 +0530

    new changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,6 +6,7 @@ function Card(props) {
     <div>
       <h2>{props.name}</h2>
       <img src={props.img} alt="avatar_img" />
+      <p>{props.desc}</p>
       <p>{props.tel}</p>
       <p>{props.email}</p>
     </div>
@@ -18,15 +19,26 @@ ReactDOM.render(
     <Card
       name="Beyonce"
       img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      desc="Singer"
       tel="+123 456 789"
       email="b@beyonce.com"
     />
     <Card
       name="Jack Bauer"
       img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+      desc="Fictional Character"
       tel="+7387384587"
       email="jack@nowhere.com"
     />
+    <Card 
+      name="Chuck Norris"
+      imgURL="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+      desc ="Actor"
+      tel = "+918 372 574"
+      email = "gmail@chucknorris.com"
+    />
+
+
   </div>,
   document.getElementById("root")
 );
